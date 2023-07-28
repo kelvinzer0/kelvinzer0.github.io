507701b57be1d604ca77e157959665067128ea3f
Kelvin Yuli Andrian <121687899+kelvinzer0@users.noreply.github.com>
2023-07-29 00:25:26 +0700

    Create worker.js

---
+++ ai2/worker.js
@@ -0,0 +1,31 @@
+// worker.js
+self.onmessage = function (event) {
+  const eventdata = JSON.parse(event.data);
+  const timestampStart = Date.now();
+  const message = eventdata.message;
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://criminalpolisheddebugmonitor--kelvin-yuliyuli.repl.co/?start_enc=true', true);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  xhr.setRequestHeader('IP-Sec-ZerolabsImpact', eventdata.ipsec);
+  xhr.onerror = function() {
+    self.postMessage({ done: false});
+  };
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 3) {
+      const newChunks = this.responseText.split('----END----').filter(Boolean);
+      const newestChunk = newChunks.pop();
+      const typing = newestChunk.replace("__99", newestChunk);
+
+      // Check if the desired condition is met
+      if (typing == "----DONE----") {
+        self.postMessage({ done: true, typing });
+      } else {
+        self.postMessage({ done: false, typing });
+      }
+    }
+  };
+
+  const data = JSON.stringify({ "message": message, "timestamp": timestampStart});
+  xhr.send(data);
+};
+
