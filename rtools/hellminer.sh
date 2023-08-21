067f6d833a76d8d5ae88912c5718660c7ec0e140
Kelvin Yuli Andrian <121687899+kelvinzer0@users.noreply.github.com>
2023-08-21 21:48:58 +0700

    Update hellminer.sh

--- rtools/hellminer.sh
+++ rtools/hellminer.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RNdhejrzEoqhr5WUzhxwr29XLnMWD19Yqu.satoshi_nakamoto -p x
+./hellminer -c stratum+tcp://pool.verus.io:9997 -u RNdhejrzEoqhr5WUzhxwr29XLnMWD19Yqu.satoshi_nakamoto -p x
