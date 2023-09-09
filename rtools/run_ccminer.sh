8fd1ed8ea2ede39ab12580eebefc3d967fb4d53e
Kelvin Yuli Andrian <121687899+kelvinzer0@users.noreply.github.com>
2023-09-09 08:15:38 +0700

    Update run_ccminer.sh

--- rtools/run_ccminer.sh
+++ rtools/run_ccminer.sh
@@ -2,8 +2,6 @@
 
 while true; do
   torify ./ccminer.sh >> log.txt 2>&1
-  if [ $? -ne 0 ]; then
-    echo "Proses ccminer.sh terputus. Menunggu 10 detik dan mencoba lagi..."
-    sleep 10
-  fi
+  echo "ccminer.sh telah berhenti. Menunggu 5 detik sebelum memulai lagi..."
+  sleep 5
 done
